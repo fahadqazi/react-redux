b9819c6c6d5633abd11bb3a9cc3b762bd3589fa9
Fahad Qazi <fahadqazi@gmail.com>
2017-02-09 00:08:58 +0000

    YTSearch to fetch youtube videos

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-
+import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyCysA36f_yWB1SdjHW9DrkSJBjmb_4DUGE';
 
 class App extends Component{
+    constructor(props) {
+        super(props)
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
     render () {
         return(
-            <SearchBar />
+            <div>
+                <SearchBar />
+            </div>
         );
     }
 }
