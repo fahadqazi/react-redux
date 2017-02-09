df9224df499261a6a66c2223e27bd5e62b7007bc
Fahad Qazi <fahadqazi@gmail.com>
2017-02-09 23:52:23 +0000

    adding function to select video from a list and play

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyCysA36f_yWB1SdjHW9DrkSJBjmb_4DUGE';
 
@@ -9,16 +10,23 @@ class App extends Component{
     constructor(props) {
         super(props)
 
-        this.state = { videos: [] };
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+         };
 
         YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos });
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+             });
         });
     }
     render () {
         return(
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.selectedVideo} />
                 <VideoList videos={this.state.videos} />
             </div>
         );
